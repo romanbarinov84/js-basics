0fc6ca5fb6cd4eb4b37c960878ade054a4cf5c1c
Roman <roman-meinl@ukr.net>
2025-02-13 15:49:20 -0200

    for files

---
+++ for of ,while/for.js
@@ -0,0 +1,19 @@
+
+
+ let carArr = [
+    ["AUDI" , "A4", 2000, "UNIVERSAL"],
+    ["BMW", "740-SL", 2001, "SEDAN"],
+    ["MERSEDES", "500 SEC", 2005, "SEDAN"],
+    ["VOLVO", "50 LONG", 1999, "SEDAN"],
+    ["SCODA", "FABIA", 2012, "HETCH"],
+    ["SEAT","IBIZA", 2008,"HETCH"],
+    ["FIAT","UNO",2004,"UNIVERSAL"]
+ ]
+
+ let i = 0;
+
+ while(i < carArr.length){
+    let res =carArr[i]
+    console.log(res);
+    i++;
+ }
